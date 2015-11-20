a531c9b21a0c259068951429996b87f2fab0999d
juanvallejo <juan.vallejo.12@cnu.edu>
2015-11-19 23:38:39 -0500

    switch api for mysql query

--- server.js
+++ server.js
@@ -613,47 +613,30 @@ function initSocketListener() {
 		 		});
 		 	}
 
-		 	var request = http.request({
-		 		host: 'pmm-rubyserverapps.rhcloud.com',
-		 		path: '/api/v1/context/' + (clientData.context || 'general') + '/id/' + clientData.id,
-		 		method: 'GET',
-		 		headers: {
-		 			'Authentication': 'email=juan.vallejo.12@cnu.edu; key=eOKpAJgR3'
+		 	// to speed things up, ignore api, use direct mysql query
+		 	mysql.query('SELECT t1.event_id, t2.event_name, t2.semester, t2.year, t3.student_id AS id, t3.first, \
+		 		t3.last, t3.major, t3.year AS gradyear, t3.email, t3.date_added AS since, t4.crn FROM `attendance` AS t1 \
+		 		LEFT JOIN `students` AS t3 ON t1.student_id=t3.student_id LEFT JOIN `events` AS t2 ON t1.event_id=t2.table_name \
+		 		LEFT JOIN `chosencourses` AS t4 ON t1.student_id=t4.student_id \
+		 		WHERE t1.student_id="' + clientData.id + '" ORDER BY t2.id ASC', function(err, rows) {
+
+		 		if(err) {
+
+		 			client.emit('registerapistudentidresponse', {
+			 			entries: [],
+			 			error: true,
+			 			message: err.toString()
+			 		});
+
+		 			return console.log('SERVER', 'CLIENT', 'MYSQL', err);
 		 		}
 
-		 	}, function(response) {
-
-		 		var data = '';
-
-		 		response.on('data', function(chunk) {
-		 			data += chunk;
-		 		});
-
-		 		response.on('end', function() {
-
-		 			try {
-		 				client.emit('registerapistudentidresponse', {
-		 					entries: JSON.parse(data),
-		 					id: clientData.id,
-		 					context: clientData.context
-		 				});
-		 			} catch(e) {
-		 				console.log('SERVER', 'CLIENT', 'HTTP', 'JSON->parse', e);
-		 			}
-
-		 		});
-
-		 	});
-
-		 	request.end();
-
-		 	request.on('error', function(err) {
-		 		console.log('SERVER', 'CLIENT', 'HTTP', 'ERR', err);
 		 		client.emit('registerapistudentidresponse', {
-		 			entries: [],
-		 			error: true,
-		 			message: err.toString()
-		 		});
+ 					entries: rows,
+ 					id: clientData.id,
+ 					context: clientData.context
+ 				});
+
 		 	});
 
 		 });
