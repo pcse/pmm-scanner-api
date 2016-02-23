ad8d1861cfcbd219eea8624241c1e7a83209cebb
juanvallejo <juan.vallejo.12@cnu.edu>
2016-02-22 20:59:48 -0500

    add sample private folder

---
+++ private/auth.js
@@ -0,0 +1,13 @@
+/**
+ * Hidden from public view, contains API server credentials
+ * for authenticating app requests
+ */
+
+var auth = {
+
+	email: 'sample.email@email.com',
+	key: 'SAMPLE'
+
+};
+
+module.exports = auth;
